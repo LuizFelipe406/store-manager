c0697b2d4e5644139a4ff6aabc2500952d2a48e8
LuizFelipe406 <luizfpereira406@gmail.com>
2022-09-12 19:49:59 -0300

    Remove comentarios

--- src/services/salesServices.js
+++ src/services/salesServices.js
@@ -1,15 +1,5 @@
 const salesModels = require('../models/salesModels');
 const productModels = require('../models/productModels');
-// const productService = require('./productServices');
-
-// const validations = async (sales) => {
-//   for (let i = 0; i < sales.length; i += 1) {
-//     if (!sales[i].productId) throw new Error('PRODUCT_ID_IS_REQUIRED');
-//     if (sales[i].quantity < 1) throw new Error('QUANTITY_INVALID');
-//     if (!sales[i].quantity) throw new Error('QUANTITY_IS_REQUIRED');
-//     await productService.getProductById(sales[i].productId);
-//   }
-// };
 
 const insertSale = async (sales) => {
   await Promise.all(sales.map(async (sale) => {
@@ -18,11 +8,8 @@ const insertSale = async (sales) => {
     if (!sale.quantity) throw new Error('QUANTITY_IS_REQUIRED');
     const [response] = await productModels.getProductById(sale.productId);
     if (!response) throw new Error('PRODUCT_NOT_FOUND');
-    // productService.getProductById(sale.productId);
     return sale;
   }));
-
-  // await validations(sales);
 
   const insertId = await salesModels.insertSale(sales);
 
